9f75bf687791bc0bb182b69ff17b1208089ef0f2
Nina Chen <n0990108@gmail.com>
2021-01-13 00:33:23 +0800

    updated file

--- password.py
+++ password.py
@@ -1,14 +1,13 @@
-password = 'a123456'
+pwd = 'a123456'
 i = 3
-while True:
-	passwordtry = input('請輸入密碼: ')
-	if passwordtry == password:
+while i > 0:
+	pwdt = input('請輸入密碼: ')
+	i = i - 1
+	if pwdt == pwd:
 		print('登入成功!')
 		break
 	else:
-		i = i - 1
 		if i == 0:
-			print('登入失敗,請過30秒再次輸入')
+			print('密碼錯誤! 帳號已被鎖定')
 			break
 		print('密碼錯誤! 還有', i, '次機會')
-		
